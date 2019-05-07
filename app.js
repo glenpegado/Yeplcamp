2f02a0013f7a80b0884a762b30e6795a13d610d5
Glen Pegado <glenpegado@gmail.com>
2019-05-06 19:14:45 -0700

    Added get request route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/yelp_camp')
+mongoose.connect('mongodb://localhost:27017/yelp_camp', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
@@ -46,12 +46,13 @@ let campgrounds = [
     {name: "Salmon creek", image: "https://cdn.vox-cdn.com/thumbor/-JoPdcgAuLTUsWiDZ62CX4wb33k=/0x0:5225x3479/1200x800/filters:focal(2195x1322:3031x2158)/cdn.vox-cdn.com/uploads/chorus_image/image/54137643/camping_tents.0.jpg"},
     {name: "San Francisco", image: "https://accelerator-origin.kkomando.com/wp-content/uploads/2016/03/camping.jpg"},
     {name: "Great Camp", image: "https://onmilwaukee.com/images/articles/ca/camping/camping_fullsize_story1.jpg?20080730123152"}
-]
+] 
 
 app.get('/', (req, res) => {
     res.render('landing')
 })
 
+//INDEX -show all camgrounds
 app.get('/campgrounds', (req, res) =>{
     // Get all campgrounds from DB
     Campground.find({}, (err, allCampgrounds)=>{
@@ -63,6 +64,7 @@ app.get('/campgrounds', (req, res) =>{
     })
 })
 
+//CREATE - add new campground to DB
 app.post('/campgrounds', (req, res) => {
     //get data from the form and add to campgrounds array
     let name = req.body.name;
@@ -85,8 +87,13 @@ app.post('/campgrounds', (req, res) => {
 //     res.send('it deleted!')
 // })
 
+//NEW - show form to create new campground
 app.get('/campgrounds/new', (req, res) =>{
     res.render('new.ejs')
+})
+
+app.get('/campgrounds/:od', (req, res) => {
+    res.send("This will be the show page one day")
 })
 
 app.listen(port, () => console.log(`The YelpCamp server has started`))
